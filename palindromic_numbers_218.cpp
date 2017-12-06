c2e54ad07235b2f8fb24835e081a2aa5e61ba1c0
Martin Nestorov <mnestorov@protonmail.com>
2017-12-06 21:16:47 +0200

    Fixed algorithm of 218, WIP

--- palindromic_numbers_218.cpp
+++ palindromic_numbers_218.cpp
@@ -40,24 +40,26 @@ int main()
   std::string pal_number = " ";
   std::cout << "Enter a number: ";
   std::cin >> pal_number;
-  for (int i = 0; i < 1000; i++)
-  {
-    std::string temp = pal_number;
-    std::reverse(pal_number.begin(), pal_number.end());
-    int num = std::stoi(temp) + std::stoi(pal_number);
 
-    if (check_pal(std::to_string(num)))
-      {
-        std::cout << num << std::endl;
-        std::abort();
-      }
+  for (int i = 0; i < 1000; i++)
+    {
+      std::string temp = pal_number;
+      std::reverse(pal_number.begin(), pal_number.end());
+      int num = std::stoi(temp) + std::stoi(pal_number);
+
+      if (check_pal(std::to_string(num)))
+        {
+          std::cout << num;
+          return 0;
+        }
     }
-  return 0;
+
+   return 0;
 }
 
 bool check_pal(std::string num)
 {
-  size_t n;
+  size_t n = 0 ;
   for (size_t i = 0; i < 1000; i++)
     {
       if (num[i] == num[1000 - i])
@@ -65,6 +67,7 @@ bool check_pal(std::string num)
           n += 1;
           if (n == num.size())
             {
+              std::cout << num;
               return true;
             }
         }
