01e0977b85dad090e9b3e3f61aea84cf9ec43cbd
Martin Nestorov <mnestorov@protonmail.com>
2017-12-14 00:03:25 +0200

    Renamed file to have its challenge number and added exception handeling

--- rack_management_294_p1.cpp
+++ rack_management_294_p1.cpp
@@ -11,8 +11,14 @@ int main()
     std::string word;
     for (auto &c : input)
     {
-         word.append(1, board.at(board.find(c)));
-         board.erase(std::find(board.begin(), board.end(), c));
+         try
+         {
+              word.append(1, board.at(board.find(c)));
+              board.erase(std::find(board.begin(), board.end(), c));
+         }
+         catch (std::exception e)
+         {
+         }
     }
 
     (word == input) ? std::cout << "true" : std::cout << "false";
