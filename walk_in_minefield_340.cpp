859ecc0ee42a390f77640039bf6477843847b23e
Martin Nestorov <mnestorov@protonmail.com>
2017-12-19 19:01:29 +0200

    Moved files

--- walk_in_minefield_340.cpp
+++ walk_in_minefield_340.cpp
@@ -6,35 +6,112 @@
 class robot
 {
 private:
-     void move(const std::string &commands);
-     void set_map(const std::vector<std::string> &map);
      bool is_started() const;
+
+     void change_state(bool s);
+     void move_n();
+     void move_s();
+     void move_e();
+     void move_w();
 
 public:
      robot();
      robot(const std::vector<std::string> &m, bool s);
 
-     void move_n();
-     void move_s();
-     void move_e();
-     void move_w();
-     void display();
+     void set_map(const std::vector<std::string> &map);
+     void move(const char &command);
+     void set_pos(size_t pos);
+     void display() const;
+
+     size_t get_position() const;
 
      std::vector<std::string> map;
-     short int position;
+     size_t position;
      bool started;
 };
 
-robot::robot()
-{
-     started = false;
-}
+robot::robot() : started(false)
+{}
 
 robot::robot(const std::vector<std::string> &m, bool s) : map(m), started(s)
 {}
 
+void robot::set_map(const std::vector<std::string> &map)
+{
+     this->map = map;
+}
+
+void robot::set_pos(size_t pos)
+{
+     position = pos;
+}
+
+size_t robot::get_position() const
+{
+     size_t curr_pos;
+     for (auto &line : map)
+     {
+          curr_pos = line.find('M');
+     }
+     return curr_pos;
+}
+
+void robot::move(const char &command)
+{
+     while (!is_started())
+     {
+          // Exit the move function if the robot is not started and skip the specific move actions
+          if (std::tolower(command) == 'i')
+          {
+               change_state(!is_started());
+          }
+     }
+     switch(std::tolower(command))
+     {
+     case 'n': move_n(); break;
+     case 's': move_s(); break;
+     case 'e': move_e(); break;
+     case 'w': move_w(); break;
+     default : std::cout << "Invalid command!" << std::endl; break;
+     }
+}
+
+inline bool robot::is_started() const
+{
+     return started;
+}
+
+void robot::move_n()
+{
+     for (auto &line : map)
+     {
+          std::cout << line.at(get_position());
+     }
+}
+
+void robot::move_s()
+{
+
+}
+
+void robot::move_e()
+{
+
+}
+
+void robot::move_w()
+{
+
+}
+
+void robot::display() const
+{
+
+}
+
 std::vector<std::string> get_map_by_line_from_file(std::string path)
 {
+     std::cout << "Hello";
      std::ifstream map(path.c_str());
      std::string line;
      std::vector<std::string> fl_map;
@@ -67,8 +144,19 @@ std::vector<std::string> get_map_by_input()
 
 int main()
 {
-     std::vector<std::string> map = get_map_by_line_from_file("map.txt");
+     std::vector<std::string> map;
+     map = get_map_by_line_from_file("map.txt");
      robot r(map, false);
+
+     std::cout << "Enter command for the robot to move: ";
+     std::string commands;
+     std::getline(std::cin, commands);
+
+     for (auto &command : commands)
+     {
+          r.move(command);
+          r.display();
+     }
 
      return 0;
 }
