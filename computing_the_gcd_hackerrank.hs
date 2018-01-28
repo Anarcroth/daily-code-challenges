5f0ad2fba11673b49feb33595e1abd01f0d647ca
Martin Nestorov <mnestorov@protonmail.com>
2018-01-28 02:49:12 -0700

    Added GCD problem from Hackerrank FP

--- computing_the_gcd_hackerrank.hs
+++ computing_the_gcd_hackerrank.hs
@@ -14,8 +14,8 @@ gcd' n m
 -- This part is related to the Input/Output and can be used as it is
 -- Do not modify it
 main = do
-  input <- getLine
-  print . uncurry gcd' . listToTuple . convertToInt . words $ input
- where
-  listToTuple (x:xs:_) = (x,xs)
-  convertToInt = map (read :: String -> Int)
+    input <- getLine
+    print . uncurry gcd' . listToTuple . convertToInt . words $ input
+    where
+    listToTuple (x:xs:_) = (x,xs)
+    convertToInt = map (read :: String -> Int)
