9526900da20fe93d6ddf93c3c744673a1aec94e2
Martin Nestorov <mnestorov@protonmail.com>
2018-02-09 18:43:38 +0200

    Removed whitespaces

--- day_of_the_programmer_hackerrank.py
+++ day_of_the_programmer_hackerrank.py
@@ -10,13 +10,13 @@ def solve(year):
     # 13.09.1918 + 13 days = 26.09.1918
     if (year == 1918):
         return "26.09."+str(year)
-    
+
     if (year <= 1917):
         if (year % 4 == 0):
             return "12.09."+str(year)
         else:
             return "13.09."+str(year)
-    
+
     if year % 4 == 0 and year % 100 != 0:
         return "12.09."+str(year)
     elif year % 400 == 0:
