0801addfd1b5a4fa7d26091e831a8b4f82fb5b10
Martin Nestorov <mnestorov@protonmail.com>
2018-02-18 15:46:20 +0200

    Removed whitespaces

--- radio_transmitters.py
+++ radio_transmitters.py
@@ -5,12 +5,38 @@ import sys
 def hackerlandRadioTransmitters(x, k):
     x.sort();
     num_transmitters = 0;
-    step = 1;
-    for index, house in enumerate(x[::step]):
-        if abs(house - x[index + k - 1]) <= k and abs(x[index + k - 1] - x[index + k*2 - 1]) <= k:
-            print("house for transmitter: ", house);
-            num_transmitters += 1;
-            step = 2*k;
+    steps = k * 2 + 1;
+    _list = [];
+    _list2 = [];
+    q = x[0];
+    while q <= x[-1]:
+        _list2.append(q);
+        q += 1;
+    i = 0;
+    z = 0;
+    while i < len(x):
+        _list.append([]);
+        for n in range(0, steps):
+            try:
+                print("x[i]: ", x[i], ", x[i + n]: ", x[i + n], ", _list2[i]: ", _list2[i], ", _lsit2[i + n]: ", _list2[i + n]);
+            except:
+                pass;
+            for er in _list2:
+                try:
+                    if (x[i + n] == _list2[i + n]):
+                        _list[z].append(x[i + n]);
+                        break;
+                except:
+                    pass;
+
+           # try:
+           #     if (x[i + n] == _list2[i + n]):
+           #         _list[z].append(x[i + n]);
+           # except:
+           #     pass;
+        z += 1;
+        i += 1;
+    print(_list);
     return num_transmitters;
 
 if __name__ == "__main__":
