77d6e701b2ae8ab162f9f6afe3941f0d45edb42a
Martin Nestorov <mnestorov@protonmail.com>
2018-02-20 20:59:07 +0200

    Added challenge descprition, WIP

--- date_dilemma_245.py
+++ date_dilemma_245.py
@@ -77,7 +77,7 @@ wrong_date =  input("Enter date: ");
 wrong_date = re.split("[\s/-]", wrong_date);
 correct_date = "";
 for date in  wrong_date:
-    if len(date) == 4 and wrong_date.index(date) == 2:
+    if len(date) == 4 or wrong_date.index(date) == 0:
         correct_date += date + "-";
         correct_date += wrong_date[0] + "-";
         correct_date += wrong_date[1];
