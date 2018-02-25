bb08b7464a8ac3e8fca7f2b56863045ace47649e
Martin Nestorov <mnestorov@protonmail.com>
2018-02-25 17:15:18 +0200

    Fixed issue where the tail of the ll was not properly getting an empty new node

--- ll_palindrom_checker.py
+++ ll_palindrom_checker.py
@@ -9,7 +9,7 @@ class node:
         return self._key;
 
     def get_next(self):
-        return self.next;
+        return self._next;
 
     def set_key(self, key):
         self._key = key;
@@ -18,84 +18,67 @@ class node:
         self._next = n_next;
 
 class linked_list:
-    def __init__(self, head, tail):
-        self._head = head;
-        self._tail = tail;
+    def __init__(self):
+        self._head = None;
+        self._tail = None;
 
-    def insert_position(val, pos):
-        _prev = node();
-        _tmp = node();
+    def insert_position(self, val, pos):
+        _prev = None;
+        _cur = self._head;
 
-        _cur = None;
+        for i in range(self, 1, pos):
+            _prev = _cur;
+            _cur = _cur.get_next();
 
-        _cur = head;
+        _tmp = node(val);
+        _prev.set_next(_tmp);
+        _tmp.set_next(_cur);
+
+    def delete_position(self, pos):
+        _cur = self._head;
+        _prev = None;
 
         for i in range(1, pos):
             _prev = _cur;
-            _cur = _cur._next;
+            _cur = _cur.get_next();
 
-        _tmp.key = val;
-        _prev._next = _tmp;
-        _tmp._next = _cur;
+        _prev.set_next(_cur.get_next);
 
-    def delete_position(pos):
-        _cur = head;
-        _prev = node();
+    def insert_start(self, val):
+        _tmp = node(val);
+        _tmp.set_next(self._head);
+        self._head = _tmp;
 
-        for i in range(1, pos):
-            _prev = _cur;
-            _cur = _cur._next;
+    def insert_node(self, val):
+        _tmp = node(val);
 
-        _prev._next = _cur._next;
-
-        del currnet;
-
-    def insert_start(val):
-        _tmp = node();
-        _tmp.key = val;
-        _tmp._next = head;
-        head = _tmp;
-
-    def insert_node(val):
-        _tmp = node();
-        _tmp.key = val;
-        _tmp._next = None;
-
-        if head is None:
-            head = _tmp;
-            tail = _tmp;
-            _tmp = None;
+        if self._head is None:
+            self._head = _tmp;
+            self._tail = _tmp;
         else:
-            _tmp._next = _tmp;
-            tail = _tmp;
-            _tmp = None;
+            self._tail.set_next(_tmp);
+            self._tail = _tmp;
 
-    def delete_first():
-        _tmp = None;
-        _tmp = head;
-        head = head_next;
+    def delete_first(self):
+        _tmp = self._head;
+        self._head = self._head.get_next();
 
-        del _tmp;
-
-    def delete_last():
-        _cur = None;
-        _prev = node();
-        _cur = head;
-        while _cur._next is not None:
+    def delete_last(self):
+        _prev = None;
+        _cur = self._head;
+        while _cur.get_next() is not None:
             _prev = _cur;
-            currnet = _cur._nextl
+            _cur = _cur.get_next();
 
-        tail = _prev;
-        _prev._next = None;
+        self._tail = _prev;
+        _prev.set_next(None);
 
-        del _cur;
-
-    def display():
-        _tmp = head;
+    def display(self):
+        _tmp = self._head;
 
         while _tmp is not None:
-            print(_tmp.key);
-            _tmp = _tmp._next;
+            print(_tmp.get_key());
+            _tmp = _tmp.get_next();
 
 if __name__ == "__main__":
     ll = linked_list();
@@ -111,7 +94,7 @@ if __name__ == "__main__":
     print("5. Quit.");
 
     while answer is not 0:
-        #answer = input("Enter action: ");
+        answer = int(input("Enter action: "));
 
         if answer == 1:
             elem = input("Enter an element: ");
