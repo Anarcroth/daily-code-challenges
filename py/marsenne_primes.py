06b90d5cab2da9bc325a3bee5a7df295c4dd999a
Martin Nestorov <mnestorov@protonmail.com>
2018-03-07 20:04:13 +0200

    Added marsenne primes

---
+++ py/marsenne_primes.py
@@ -0,0 +1,16 @@
+#!bin/python3
+
+def is_prime(n):
+    prime_range = []
+    for x in range(1, n + 1):
+        if n % x == 0:
+            prime_range.append(x)
+
+    if [1, n] == prime_range:
+        return True
+
+p = 1
+while p <= 31:
+    if is_prime(pow(2, p) - 1):
+        print(p, ":", pow(2, p) - 1, "is a Marsenne prime.")
+    p += 1
