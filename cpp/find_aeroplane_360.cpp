d5753cb0b32453656ebd80eda15acf13522bb904
Martin Nestorov <mnestorov@protonmail.com>
2018-07-12 18:55:13 +0300

    Added curl request handle and function prototypes

--- cpp/find_aeroplane_360.cpp
+++ cpp/find_aeroplane_360.cpp
@@ -42,12 +42,52 @@
 // Replace your distance function with the geodesic distance formula, which is more accurate on the Earth's surface.
 
 #include <iostream>
-#include <nlohmann/json>
+#include <string>
+#include <nlohmann/json.hpp>
+#include <curl/curl.h>
 
 using json = nlohmann::json;
 
+void get_geodesic_distance();
+void get_callsign();
+void get_lattitude_and_longitutde();
+void get_country_of_origin();
+void get_ICA024ID();
+
+namespace
+{
+    std::size_t callback(
+        const char* IN,
+        std::size_t size,
+        std::size_t num,
+        std::string* out)
+    {
+        const std::size_t TOTAL_BYTES(size * num);
+        out->append(IN, TOTAL_BYTES);
+        return TOTAL_BYTES;
+    }
+}
+
 int main()
 {
-    json j;
+    const std::string AEROPLANE_DATA_URL("https://opensky-network.org/api/states/all");
+
+    CURL* curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, AEROPLANE_DATA_URL.c_str());
+    curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 20);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+    int http_code(0);
+    std::unique_ptr<std::string> http_data(new std::string());
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, http_data.get());
+
+    curl_easy_perform(curl);
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+    curl_easy_cleanup(curl);
+
     return 0;
 }
